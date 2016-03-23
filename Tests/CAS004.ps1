dd75be5e8c22ec8bf341f0ea0b3805202429681d
damianscoles <damian@scolesfamily.net>
2016-03-23 09:42:47 -0500

    Fixed - Test is now RC4
    Added RC4 checking code, removed SSL 3.0 code which was accidentally
    copied in.

--- Tests/CAS004.ps1
+++ Tests/CAS004.ps1
@@ -13,11 +13,11 @@ Function Run-CAS004()
     $FailedList = @()
     $ErrorList = @()
 
-    foreach ($server in $exchangeservers) {
+    foreach ($server in $ExchangeServers) {
         # Set the inital value
         $name = $server.name
         $up = $true
-        $success = $false
+        $success = $true
         try {
             $Registry = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey("LocalMachine",$name)
         } catch {
@@ -25,38 +25,66 @@ Function Run-CAS004()
         }
 
         if ($up -eq $true) {
-            
-            # Check the registry path
-            $check1 = $registry.OpenSubKey("System\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0")
-
+    
+            # Check the registry path - Cipher Stack
+            $check1 = $registry.OpenSubKey("SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\")
+    
             if ($check1 -ne $null) {
-                # Check the next registry pat
-                $check2 = $registry.OpenSubKey("System\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server")
-        
-                if ($check2 -ne $null) {
-
-                    # Check to see if Enabled value is present with a '0' value
-                    $check3 = $registry.OpenSubKey("System\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Protocols\SSL 3.0\Server").GetValue("Enabled")
-                    if ($Check3 -eq "0") {
-                        $Success = $true
-                    } else {
-                        $Success = $false
+           
+                # Registry check - RC4 128/128\
+                $check2a = $registry.OpenSubKey("SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\RC4 128/128\")
+            
+                if ($check2a -ne $null) {
+                    $check2b = $registry.OpenSubKey("SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\RC4 128/128\").GetValue("Enabled")
+                
+                    if ($check2b -ne 0) {
+                        $success = $false
                     }
+                } else {
+                    $success = $false
                 }
-            }
-        
-        
-            # Decide if the test has failed based off of the values missing or present and what value is there if present
-            If ($Success -eq $true) {
-                $PassedList += $name
-                write-verbose "SSL 3.0 is disabled on the server $name!!"
+
+                # Registry check - RC4 40/128\
+                $check3a = $registry.OpenSubKey("SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\RC4 40/128\")
+            
+                if ($check3a -ne $null) {
+                    $check3b = $registry.OpenSubKey("SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\RC4 40/128\").GetValue("Enabled")
+                
+                    if ($check3b -ne 0) {
+                        $success = $false
+                    }
+                } else {
+                    $success = $false
+                }
+
+                # Registry check 4 - RC4 56/128\
+                $check4a = $registry.OpenSubKey("SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\RC4 56/128\")
+            
+            if ($check4a -ne $null) {
+                $check4b = $registry.OpenSubKey("SYSTEM\CurrentControlSet\Control\SecurityProviders\SCHANNEL\Ciphers\RC4 56/128\").GetValue("Enabled")
+                
+                if ($check4b -ne 0) {
+                    $success = $false
+                }
             } else {
-                $FailedList += $name
-                write-verbose "SSL 3.0 is not disabed on server $name!!"
+                $success = $false
+            }
+        } else {
+            $succes = $false
+            write-verbose "The server $name is down or unreachable."
+        }
+        
+        # Decide if the test has failed based off of the values missing or present and what value is there if present
+        If ($Success -eq $true) {
+            $PassedList += $name
+            write-verbose "RC4 is disabled on the server $name!!"
+        } else {
+            $FailedList += $name
+                write-verbose "RC4 is not disabed on server $name!!"
             }
         } else {
             $FailedList += $name
-            write-verbose "The server $name is down and SSL 3.0 settings cannot be verified."
+            write-verbose "The server $name is down/unreachable and RC4 settings cannot be verified."
         }       
     }
 
