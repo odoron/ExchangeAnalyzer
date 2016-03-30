fee4d0a6f78e13a086d39a3b59953f02b2f2e256
Paul Cunningham <paul@locklan.com.au>
2016-03-30 14:44:30 +1000

    Modified some variable names for clarity

--- Tests/WSSRV001.ps1
+++ Tests/WSSRV001.ps1
@@ -17,41 +17,47 @@ Function Run-WSSRV001()
     $ErrorList = @()
 
     foreach ($server in $exchangeservers) {
-        $name = $server.name
+        $ServerName = $server.name
         if ($server.AdminDisplayVersion -match "Version 14") {$ver = "V14"}
         if ($server.AdminDisplayVersion -match "Version 15") {$ver = "V15"}
 
-        Write-Verbose "Checking $name."
+        Write-Verbose "Checking $ServerName."
 
         # Null out variables for clean results
         $installpath = $null
         $reg = $null
 
         # Get the Exchange Install Drive
-        try {
-            $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey("LocalMachine",$name)
-        } catch {
+        try
+        {
+            $reg = [Microsoft.Win32.RegistryKey]::OpenRemoteBaseKey("LocalMachine",$ServerName)
+        }
+        catch
+        {
             Write-Warning $_.Exception.Message
             $installpath = "Unable to connect to registry"
         }
-        if (!($installpath)) {
+
+        if (!($installpath))
+        {
             $installpath = $reg.OpenSubKey("SOFTWARE\Microsoft\ExchangeServer\$ver\Setup").GetValue("MsiInstallPath")
         }
-        $ExchangeDrive = $InstallPath.substring(0,2)
+        $ExchangeInstallDrive = $InstallPath.substring(0,2)
 
         # Get free Space for all drives on the Exchange Server
-        $space = Get-WMIObject win32_logicaldisk -computername $name
+        $LogicalDisks = Get-WMIObject win32_logicaldisk -computername $ServerName
     
         # Go through each drive on the server
-        foreach ($line in $space) {
+        foreach ($LogicalDisk in $LogicalDisks)
+        {
 
             # Get the current drive in the loop
-            $drive = $line.DeviceID
+            $drive = $LogicalDisk.DeviceID
 
             # Look just for the Exchange Install Drive for percent free space
-            if ($drive -eq $ExchangeDrive) {
-                $free = $line.freespace
-                $size = $line.size
+            if ($drive -eq $ExchangeInstallDrive) {
+                $free = $LogicalDisk.freespace
+                $size = $LogicalDisk.size
 
                 # Calculate percent free space
                 $percent2 = ($free/$size)*100
@@ -62,15 +68,15 @@ Function Run-WSSRV001()
                 # Check to see if the Exchange Install Drive has 30% or greater free space
                 if ($percent -lt "15") {
                     write-verbose "The Exchange Install Drive has less than 30% free space."
-                    $FailedList += $($name)
+                    $FailedList += $($ServerName)
                 } 
                 if (($percent -gt "15") -and ($percent -lt "30")) {
                    write-verbose "The Exchange Install Drive has greater than 15% and less than 30% free space."
-                    $WarningList += $($name)
+                    $WarningList += $($ServerName)
                 }
                 if ($percent -gt "30") {
                     write-verbose "The Exchange Install Drive has greater than 30% free space."
-                    $PassedList += $($name)
+                    $PassedList += $($ServerName)
                 }
             }
         }
